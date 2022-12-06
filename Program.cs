0b8c2d4413bdd756909553f708f7304f2087c6a3
battleship9 <fekete.kocsis500@gmail.com>
2022-12-06 12:59:53 +0100

    part 1 final

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+
+string rawData = File.ReadAllText("rawData.txt");
+
+Match match = Regex.Match(rawData, @"(([a-z])(?!(?:[a-z]{0,2}\2[a-z]{0,2}))([a-z])(?!(?:[a-z]{0,2}\3[a-z]{0,2}))([a-z])(?!(?:[a-z]{0,2}\4[a-z]{0,2}))([a-z])(?!(?:[a-z]{0,2}\5[a-z]{0,2})))");
+
+Console.WriteLine(match.Index + 4);
