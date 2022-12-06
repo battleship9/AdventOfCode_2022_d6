f38d856003dd1bd5a3a9eef2a7ac07e047f003e6
battleship9 <fekete.kocsis500@gmail.com>
2022-12-06 19:41:05 +0100

    part 1 final fix

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 
 string rawData = File.ReadAllText("rawData.txt");
 
-Match match = Regex.Match(rawData, @"(([a-z])(?!(?:[a-z]{0,2}\2[a-z]{0,2}))([a-z])(?!(?:[a-z]{0,2}\3[a-z]{0,2}))([a-z])(?!(?:[a-z]{0,2}\4[a-z]{0,2}))([a-z])(?!(?:[a-z]{0,2}\5[a-z]{0,2})))");
+Match match = Regex.Match(rawData, @"(([a-z])(?!(?:\2[a-z]{2}|[a-z]{1}\2[a-z]{1}|[a-z]{2}\2))([a-z])(?!(?:\3[a-z]{1}|[a-z]{1}\3))([a-z])(?!(?:\4))([a-z]))");
 
 Console.WriteLine(match.Index + 4);
